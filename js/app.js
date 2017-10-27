69431eea33c31aea4d03d2858ab68a096a98d9e4
Overtorment <overtorment@gmail.com>
2017-10-27 23:57:00 +0300

    FIX: faster load time

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ var symbol = "?";
 var contractAddress = '0x9e77d5a1251b6f7d456722a6eac6d2d5980bd891';
 
 function startApp(){
-
+  var vi
   var abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"standard","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"decimalUnits","type":"uint8"},{"name":"tokenSymbol","type":"string"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}]
 
 
@@ -82,8 +82,7 @@ function startApp(){
     decimals = rz
   })
 
-  setVariableInterval(function(){
-
+  vi = setVariableInterval(function(){
     web3.eth.getAccounts(function(error, accounts){
       if (error) return;
       myContractInstance.balanceOf(accounts[0], function(err, rez) {
@@ -91,9 +90,10 @@ function startApp(){
         $("#mybalance").val('' + rez.toString(10) );
       })
     })
-  }, 3000);
+  }, 100);
+  vi.interval = 5000;
 
-  setVariableInterval(function(){
+  vi = setVariableInterval(function(){
     web3.eth.getAccounts(function(error, accounts){
       if (error) return;
       web3.eth.getBalance(accounts[0], function(err,rez){
@@ -103,22 +103,25 @@ function startApp(){
       })
 
     })
-  }, 3000);
+  }, 100);
+  vi.interval = 5000;
 
-  setVariableInterval(function(){
+  vi = setVariableInterval(function(){
     web3.eth.getAccounts(function(error, accounts){
       if (error) return;
       $("#myaddress").text(accounts[0]);
       $("#viewOnEtherscan").attr('href', 'https://etherscan.io/address/' + accounts[0]);
     })
-  }, 3000);
+  }, 100);
+  vi.interval = 5000;
 
-  setVariableInterval(function(){
+  vi = setVariableInterval(function(){
     $("#contractaddress").text(contractAddress)
-  }, 3000);
+  }, 100);
+  vi.interval = 5000;
 
 
-  setVariableInterval(function(){
+  vi = setVariableInterval(function(){
     var checkTime = function (i) {
       if (i < 10) i = "0" + i;
       return i;
@@ -131,10 +134,11 @@ function startApp(){
     s = checkTime(s);
 
     $("#currenttime").text(h + ":" + m + ":" + s)
-  }, 1000);
+  }, 100);
+  vi.interval = 1000;
 
 
-  setVariableInterval(function(){
+  vi = setVariableInterval(function(){
     web3.version.getNetwork((err, netId) => {
       switch (netId) {
       case "1":
@@ -153,7 +157,8 @@ function startApp(){
         $("body").empty().append('please switch to Main and reload this Dapp');
       }
     })
-  }, 3000);
+  }, 100);
+  vi.interval = 5000;
 }
 
 
